b56ff98eef95a7a32ce34a8c4d3418898d36fc63
IjuYuika <122337219+IjuYuika@users.noreply.github.com>
2023-01-18 14:44:57 +0900

    Help画面の描画３
    文章の追加

--- team_c/HELP.cpp
+++ team_c/HELP.cpp
@@ -15,11 +15,13 @@ void HELP::DrawHelp() {
 	DrawString(20, 40, "ヘルプ", 0xffffff, 0);
 	SetFontSize(40);
 	DrawString(30, 150, "これは対象のターゲットがいくつ画面上にあるのかを\n見つけるゲームです", 0xffffff, 0);
+	DrawString(30, 235, "正解すると次のステージに進めます", 0xffffff, 0);
+	DrawString(30, 280, "間違えるとゲームオーバーです", 0xff0000, 0);
 
 	DrawString(20, 400, "操作説明", 0xffffff, 0);	
 
 	SetFontSize(30);
-	DrawString(430, 650, "--- Aを押してタイトルへ戻る ---", 0xffffff, 0);
+	DrawString(430, 680, "--- Aを押してタイトルへ戻る ---", 0xffffff, 0);
 
 	if (input.Buttons[12] == 1 && Button_flg == FALSE) {
 		Button_flg = TRUE;
