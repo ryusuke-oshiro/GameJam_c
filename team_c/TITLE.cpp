6f07fd33b6cb86f2f5ac3761a005dbb1ab6aa732
川平花 <hana-kawahira-k21h@stu.kbc.ac.jp>
2023-01-18 17:07:12 +0900

    マージ(手動)

--- team_c/TITLE.cpp
+++ team_c/TITLE.cpp
@@ -8,10 +8,50 @@ TITLE::TITLE() {
 }
 
 void TITLE::DrawTitle() {
-	DrawCircle(640, 360, 100, 0xffffff, true);
-	DrawFormatString(50, 10, 0x000000, "TITLE");
-	
-	if (input.Buttons[12] == 1) {
-		g_GameState = 2;
+	static int menuNo = 0;
+	int posY;
+
+
+	if (input.Buttons[1] == 1 && Button_flg == FALSE) {
+		Button_flg = TRUE;
+		if (++menuNo > 2) menuNo = 0;
 	}
+	if (input.Buttons[0] == 1 && Button_flg == FALSE) {
+		Button_flg = TRUE;
+		if (--menuNo < 0) menuNo = 2;
+	}
+	if (input.Buttons[1] == 0 && input.Buttons[0] == 0 && input.Buttons[12] == 0) {
+		Button_flg = FALSE;
+	}
+
+	if (input.Buttons[12] == 1 && Button_flg == FALSE) {
+		Button_flg = TRUE;
+		switch (menuNo) {
+		case 0:
+			g_GameState = 4;
+			break;
+		case 1:
+			g_GameState = 1;
+			break;
+		case 2:
+			g_GameState = 3;
+			break;
+		}
+
+	}
+
+
+	posY = menuNo * 100;
+	DrawTriangle(510, 310 + posY, 540, 330 + posY, 510, 350 + posY, 0xffffff, TRUE);
+
+	SetFontSize(150);
+	DrawString(500, 50, "TITLE", 0xffffff, 0);
+
+	SetFontSize(60);
+	DrawString(600, 300, "START", 0xffffff, 0);
+	DrawString(600, 400, "HELP", 0xffffff, 0);
+	DrawString(600, 500, "END", 0xffffff, 0);
+
+	DrawFormatString(300, 600, 0xffffff, "HIGH SCORE:%d", menuNo);
+
 }
