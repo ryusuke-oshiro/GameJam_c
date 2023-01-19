aee0accb63b648aa8af03d3744846b58addf9e5f
justin101314 <107020743+justin101314@users.noreply.github.com>
2023-01-19 13:46:26 +0900

    エンド,ヘルプ更新
    エンディング,操作説明

--- team_c/END.cpp
+++ team_c/END.cpp
@@ -12,7 +12,7 @@ END::END() {
 void END::DrawEnd() {
 
 	//エンディング表示
-	if (++endwaittime < 2500) endPosY = 300 - endwaittime / 2;
+	if (++endwaittime < 2300) endPosY = 300 - endwaittime / 2;
 
 	SetFontSize(80);
 	DrawFormatString(140, 5 + endPosY, 0xfffff, "Thank you for playing!!!");
@@ -27,16 +27,12 @@ void END::DrawEnd() {
 	DrawFormatString(620, 550 + endPosY, 0xffffff, "幸地　涼耶");
 
 
-
-
-
-
 	//タイム加算処理&終了
-	if (++endwaittime > 2500)g_GameState = 99;
+	if (++endwaittime > 2300)g_GameState = 99;
 
 	if (endwaittime > 2000) {
-		SetFontSize(40);
-		DrawFormatString(640, 320, 0xffd700, "END");
+		SetFontSize(200);
+		DrawFormatString(520, 260, 0xffd700, "END");
 	}
 
 
--- team_c/HELP.cpp
+++ team_c/HELP.cpp
@@ -13,14 +13,15 @@ void HELP::DrawHelp() {
 	}
 
 	SetFontSize(70);
-	DrawString(20, 40, "ヘルプ", 0xffffff, 0);
+	DrawString(20, 40, "ヘルプ", 0xffff00, 0);
 	SetFontSize(40);
 	DrawString(30, 150, "これは対象のターゲットがいくつ画面上にあるのかを\n見つけるゲームです", 0xffffff, 0);
 	DrawString(30, 235, "正解すると次のレベルに進みます", 0xffffff, 0);
 	DrawString(30, 280, "間違えるとゲームオーバーです", 0xff0000, 0);
 
-	DrawString(20,400, "操作説明", 0xffffff, 0);
-	DrawString(25,450,"Aボタンで決定",0xfff,0);
+	DrawString(20,400, "操作説明", 0x9932cc, 0);
+	DrawString(25,450, "A",0x32cd32, 0);
+	DrawString(45,450,"ボタンで決定",0xfff,0);
 	DrawString(15,490,"↑十字キー,↓十字キーで操作", 0xff, 0);
 	
 	SetFontSize(30);
