302cb90e8bb0b52a97a67b24399d78b7d68d26dd
justin101314 <107020743+justin101314@users.noreply.github.com>
2023-01-19 13:19:27 +0900

    エンディング更新Ver2

--- team_c/END.cpp
+++ team_c/END.cpp
@@ -6,26 +6,39 @@ END::END() {
 	count = 0;
 	endwaittime = 0;
 	endPosY = 0;
-	
+
 }
 
 void END::DrawEnd() {
-	
+
 	//エンディング表示
-	if (++endwaittime < 600) endPosY = 300 - endwaittime / 2;
+	if (++endwaittime < 2500) endPosY = 300 - endwaittime / 2;
 
 	SetFontSize(80);
 	DrawFormatString(140, 5 + endPosY, 0xfffff, "Thank you for playing!!!");
-    SetFontSize(50);
-	DrawFormatString(640, 170 + endPosY, 0xffffff, "Cチームメンバー");
-	DrawFormatString(540, 220 + endPosY, 0xffffff, "☆リーダー　大城　竜輔");
-	DrawFormatString(640, 280 + endPosY, 0xffffff, "宮城　ジャスティン");
-	DrawFormatString(640, 360 + endPosY, 0xffffff, "川平　花");
-	DrawFormatString(640, 420 + endPosY, 0xffffff, "棚原　千裕");
-	DrawFormatString(640, 470 + endPosY, 0xffffff, "伊集 唯華");
-	DrawFormatString(640, 520 + endPosY, 0xffffff, "幸地　涼耶");
+	SetFontSize(40);
+	DrawFormatString(580, 160 + endPosY, 0xffd700, "Cチームメンバー");
+	SetFontSize(30);
+	DrawFormatString(430, 220 + endPosY, 0xffffff, "リーダー :  大城　竜輔");
+	DrawFormatString(620, 280 + endPosY, 0xffffff, "宮城　ジャスティン");
+	DrawFormatString(620, 350 + endPosY, 0xffffff, "川平　花");
+	DrawFormatString(620, 420 + endPosY, 0xffffff, "棚原　千裕");
+	DrawFormatString(620, 480 + endPosY, 0xffffff, "伊集  唯華");
+	DrawFormatString(620, 550 + endPosY, 0xffffff, "幸地　涼耶");
+
+
+
+
+
 
 	//タイム加算処理&終了
-	if (++endwaittime > 900)g_GameState = 99;
+	if (++endwaittime > 2500)g_GameState = 99;
+
+	if (endwaittime > 2000) {
+		SetFontSize(40);
+		DrawFormatString(640, 320, 0xffd700, "END");
+	}
+
+
 
 }
